d9dfb57d6f24a3f9f3cfa81e92a49d25df21ffa0
nidith <nidith@usa.com>
2024-04-30 18:21:49 +0530

    added @app.get(/) to main.py

--- app/main.py
+++ app/main.py
@@ -13,6 +13,6 @@ app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
 
-# @app.get("/")  
-# async def root():
-#     return {"message": "Hello ALL, Welcome to the World Of APIs"}
+@app.get("/")  
+async def root():
+    return {"message": "Hello ALL, Welcome to the World Of APIs"}
