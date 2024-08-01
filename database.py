ccf33f641a1748e40c6083a67b369a6edf938a11
Nidith <155984697+0xfarben@users.noreply.github.com>
2024-08-01 19:05:46 +0530

    Update database.py -> removing f-string "SQLALCHEMY_DATABASE_URL" due to render issues.

--- database.py
+++ database.py
@@ -5,13 +5,20 @@ from sqlalchemy.orm import sessionmaker
 from config import settings
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserverorhost/db_name"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{
-    settings.DATABASE_USERNAME}:{
-        settings.DATABASE_PASSWORD}@{
-            settings.DATABASE_HOSTNAME}:{
-                settings.DATABASE_PORT}/{
-                    settings.DATABASE_NAME}"
-
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{
+#     settings.DATABASE_USERNAME}:{
+#         settings.DATABASE_PASSWORD}@{
+#             settings.DATABASE_HOSTNAME}:{
+#                 settings.DATABASE_PORT}/{
+#                     settings.DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://"
+    + settings.DATABASE_USERNAME + ":"
+    + settings.DATABASE_PASSWORD + "@"
+    + settings.DATABASE_HOSTNAME + ":"
+    + str(settings.DATABASE_PORT) + "/"
+    + settings.DATABASE_NAME
+)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, 
